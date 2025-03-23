2090bd60628cdd156e68ea591052de12073a5593
Shanmukh-235 <shanmukhapoornachand14316@gmail.com>
2025-03-23 16:29:29 +0530

    assets added

---
+++ script.js
@@ -0,0 +1,6 @@
+function jump() {
+    document.querySelector("marquee").style.transform="translate(0px,-300px)"
+}
+function down() {
+    document.querySelector("marquee").style.transform="translate(0px,0px)"
+}
